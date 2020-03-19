56dee6ea7f1f67cc9858a71e2a21ef9dac1ce40d
bluesky <goming49@gmail.com>
2020-03-19 05:21:17 -0400

    update csvtosql

--- src/csvTomysql.py
+++ src/csvTomysql.py
@@ -14,13 +14,13 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE IF NOT EXISTS csv_db")
+mycursor.execute("CREATE DATABASE IF NOT EXISTS OECD_DB")
 script_dir = os.path.abspath(os.path.dirname(sys.argv[0]) or '.') 
 csv_path = os.path.join(script_dir, 'DATA_CSV')
 os.chdir(csv_path)
 extension = "csv"
 all_filenames = [i for i in glob.glob('*{}'.format(extension))]
-mydb = mysql.connector.connect(host = "localhost", user = 'root', passwd = '', database='csv_db', port="3306")
+mydb = mysql.connector.connect(host = "localhost", user = 'root', passwd = '', database='OECD_DB', port="3306")
 cursor = mydb.cursor()
 print(all_filenames)
 for filename in all_filenames:
@@ -35,40 +35,52 @@ for filename in all_filenames:
             for index in range(1, len(row)):
                 data.append(row[index])
             datas.append(data)
-
-        creatQueryString = ""
-        valFormat = ""
-        fieldName = ""
-        insertQueryString = 'INSERT INTO `' + filename.replace('.csv', '').lower() + '`('
+        
         i = 1
         while 1:
-            tit = titles[20*(i-1):20*i]
-            if not tit:
-                break
-            for string in tit:
-                creatQueryString += "`" + str(string) + "`" + " TEXT,"
-                valFormat += "%s,"
-                fieldName += "`" + str(string) + "`" +", "
-            valFormat = valFormat[:-1]
-            fieldName = fieldName[:-2]
-            creatQueryString = creatQueryString[:-1]
-            insertQueryString = insertQueryString + fieldName +") " + "VALUES (" + valFormat + ")"
-            if len(titles) < 20:
+            createQueryString = ""
+            valFormat = ""
+            fieldName = ""
+            if len(titles) < 1000:
                 index = ""
             else:
                 index = "("+str(i)+")"
-            creatQueryString = "CREATE TABLE IF NOT EXISTS `" + filename.replace('.csv','').lower() + index + "`(" + creatQueryString +")"
+            insertQueryString = 'INSERT INTO `' + filename.replace('.csv', '').lower() + index + '`('
+            tit = titles[1000*(i-1):1000*i]
+            if not tit:
+                break
+            for string in tit:
+                createQueryString += "`" + str(string) + "`" + " TEXT,"
+                valFormat += "%s,"
+                fieldName += "`" + str(string) + "`" +", "
             
-            cursor.execute(creatQueryString)
-            x = []
-            for j in range(len(datas[0])):
-                x.append([datas[i][j] for i in range(20*(i-1),20*i)])
+            valFormat = valFormat[:-1]
+            fieldName = fieldName[:-2]
+            createQueryString = createQueryString[:-1]
+            insertQueryString = insertQueryString + fieldName +") " + "VALUES (" + valFormat + ")"
+            
+            createQueryString = "CREATE TABLE IF NOT EXISTS `" + filename.replace('.csv','').lower() + index + "`(" + createQueryString +") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+            # cursor.execute('ALTER TABLE' + filename.replace('.csv','').lower() + index + 'ROW_FORMAT=COMPRESSED ')
+            cursor.execute("SET GLOBAL innodb_default_row_format='dynamic'")
+            cursor.execute("SET SESSION innodb_strict_mode=OFF")
+            cursor.execute(createQueryString)
+            
+            # x = []
+            tran = [list(j) for j in zip(*datas)]
+            x = [m[1000*(i-1):1000*i] for m in tran if m[1000*(i-1):1000*i]]
+            # for j in range(len(datas[0])):
+            #     x = [m[1000*(i-1),1000*i] for m in datas if m[1000*(i-1),1000*i]]
+            # print(len(x))
             result = []
-            for i in x:
-                result.append(tuple(i))
-            for i in range(0,len(result)):
-                if result[100*i:100*(i+1)]:
-                    cursor.executemany(insertQueryString, result[100*i:100*(i+1)])
+            for j in x:
+                result.append(tuple(j))
+            j = 0
+            while 1:
+                if result[100*j:100*(j+1)]:
+                    cursor.executemany(insertQueryString, result[100*j:100*(j+1)])
+                else:
+                    break
+                j += 1
             mydb.commit()
             i += 1
     print("importing successfully finished")
